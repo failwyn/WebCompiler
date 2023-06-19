31fb5cafa248fae76b27c86f65f965b1b9a55f0b
Gary Hunt <gary.hunt@datadeliverynetwork.com>
2023-06-19 23:09:34 +0100

    Extract the files in *nix

--- src/WebCompiler/Compile/CompilerService.cs
+++ src/WebCompiler/Compile/CompilerService.cs
@@ -95,17 +95,35 @@ namespace WebCompiler
                     Directory.CreateDirectory(_path);
                     SaveResourceFile(_path, "WebCompiler.Node.node.7z", "node.7z");
                     SaveResourceFile(_path, "WebCompiler.Node.node_modules.7z", "node_modules.7z");
-                    SaveResourceFile(_path, "WebCompiler.Node.7z.exe", "7z.exe");
-                    SaveResourceFile(_path, "WebCompiler.Node.7z.dll", "7z.dll");
-                    SaveResourceFile(_path, "WebCompiler.Node.prepare.cmd", "prepare.cmd");
+
+                    string processFileName;
+                    string processArguments;
+                    switch ( System.Environment.OSVersion.Platform )
+                    {
+                        case PlatformID.Unix:
+                            case PlatformID.MacOSX:
+                            SaveResourceFile(_path, "WebCompiler.Node.prepare.sh", "prepare.sh");
+                            processFileName = "/bin/bash";
+                            processArguments = "prepare.sh";
+                            break;
+
+                        default:
+                            SaveResourceFile(_path, "WebCompiler.Node.7z.exe", "7z.exe");
+                            SaveResourceFile(_path, "WebCompiler.Node.7z.dll", "7z.dll");
+                            SaveResourceFile(_path, "WebCompiler.Node.prepare.cmd", "prepare.cmd");
+                            processFileName = "cmd.exe";
+                            processArguments = "/c prepare.cmd";
+                            break;
+                    }
+
 
                     ProcessStartInfo start = new ProcessStartInfo
                     {
                         WorkingDirectory = _path,
                         CreateNoWindow = true,
                         WindowStyle = ProcessWindowStyle.Hidden,
-                        FileName = "cmd.exe",
-                        Arguments = "/c prepare.cmd"
+                        FileName = processFileName,
+                        Arguments = processArguments
                     };
 
                     Process p = Process.Start(start);
--- src/WebCompiler/Node/prepare.cmd
+++ src/WebCompiler/Node/prepare.cmd
@@ -7,4 +7,3 @@ del /q node_modules.7z
 del /q 7z.exe
 del /q 7z.dll
 del /q prepare.cmd
-del /q prepare.sh
